04bd9513b84edd7485e868cc0c5524627675bd43
John <nmamadaliyev@list.ru>
2021-02-21 14:06:55 +0500

    SBMS

---
+++ script.js
@@ -0,0 +1,12 @@
+function funksiya(){
+    document.getElementById('asd').style.backgroundColor = '#3D3D9A';
+    document.getElementById('text').style.color = '#fff';
+}
+
+function show(){
+    document.getElementById('numbers').style.display = 'block';
+}
+
+function cancel(){
+    document.getElementById('numbers').style.display = 'none';
+}
